02f794035c9e1517352d8a3ce824eda74914006e
Jeffrey Snijder <j.snijder@out-smart.nl>
2019-01-08 12:52:33 +0100

    Wat doet dit, nu ik met tien vingers tik

--- TestGithub/AppDelegate.swift
+++ TestGithub/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        print ("Brood")
+        print ("Blauw")
         // Override point for customization after application launch.
         return true
     }
